cad6f2e4f2a1c69090fa54293d47cdf105a1b909
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2013-06-10 11:38:18 -0700

    Move more configuration to meta tags

--- helpers/block_helper.rb
+++ helpers/block_helper.rb
@@ -4,7 +4,7 @@ module BlockHelper
   # A more haml-esque way to write tumblr block tags.
   def block( name, &block )
     name  = name.to_s.camelize
-    open  = "{block:#{name}}"
+    open  = "{block:#{name}}\n"
     close = "{/block:#{name}}"
     open + capture_haml( &block ) + close
   end
